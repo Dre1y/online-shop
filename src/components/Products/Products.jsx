ff15d3251834ff0ca36d3eb2505505eb786f046d
Andrey <andreymatheus92@gmail.com>
2024-10-08 16:38:38 -0300

    feat: add loading logic

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,17 +3,22 @@ import React, { useEffect, useState } from "react";
 import "./Products.css";
 import fetchProducts from "../../api/fetchProducts";
 import ProductCard from "../ProductCard/ProductCard";
+import Loading from "../Loading/Loading";
 
 function Produts() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts("motorola").then((resp) => {
       setProducts(resp);
+      setLoading(false);
     });
   }, []);
 
-  return (
+  return loading ? (
+    <Loading />
+  ) : (
     <section className="products container">
       {products.map((product) => (
         <ProductCard key={product.id} data={product} />
